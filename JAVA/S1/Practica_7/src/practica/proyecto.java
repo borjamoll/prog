8d78ee1f6fb9a8c0c81856c2893672f24e60c9cc
Dani Apesteguia <alumnedani@gmail.com>
2020-04-04 19:44:18 +0200

    Al fin funciona.

--- JAVA/S1/Practica_7/src/practica/proyecto.java
+++ JAVA/S1/Practica_7/src/practica/proyecto.java
@@ -10,13 +10,15 @@ public class proyecto {
         String[] informacion_pelicula = {"-----", "Año: ", "Director: ", "Duración: ", "Sinopsis: ", "Reparto: ", "Sesión: "};
         int recorrer = 0;
         try {
-            //Leer el archivo
-            System.out.println("--------------------------------------");
-            System.out.println(" ");
-            System.out.println("Cartelera de CineFBMoll");
-            System.out.println(" ");
-            System.out.println("--------------------------------------");
             FileInputStream archivo_lectura = new FileInputStream("C:/Users/alumn/Desktop/X/Programacion/JAVA/S1/Practica_7/src/practica/rafa.txt");
+            FileWriter escritura = new FileWriter("C:/Users/alumn/Desktop/X/Programacion/JAVA/S1/Practica_7/src/practica/rafa_final.txt");
+            PrintWriter printwriter = new PrintWriter(escritura);
+            
+            printwriter.println("--------------------------------------");
+            printwriter.println(" ");
+            printwriter.println("Cartelera de CineFBMoll");
+            printwriter.println(" ");
+            printwriter.println("--------------------------------------");
             int c = 0;
             while (c != -1) {
                 char letra = (char) c; //Convierte el valor en bits en su carácter
@@ -24,25 +26,26 @@ public class proyecto {
                     recorrer = 0;
                 } else if (c == 35 || recorrer == 0) { //El carácter 35 es el carácter #
                     if (recorrer == 1) { //Para imprimir las letras de la película
-                        System.out.print("-----");
-                        System.out.println(" ");
-                        System.out.println(" ");
-                        System.out.print(informacion_pelicula[recorrer]);
+                        printwriter.print("-------");
+                        printwriter.println(" ");
+                        printwriter.println(" ");
+                        printwriter.print(informacion_pelicula[recorrer]);
                     } else { //Para poner si es el título, año, director, género...)
-                        System.out.println(" ");
-                        System.out.println(" ");
-                        System.out.print(informacion_pelicula[recorrer]);
+                        printwriter.println(" ");
+                        printwriter.println(" ");
+                        printwriter.print(informacion_pelicula[recorrer]);
                         if (c != 35) { //Esta condición es para imprimir la primera letra de la película.
-                            System.out.print(letra);
+                            printwriter.print(letra);
                         }
                     }
                     recorrer++;
                 } else {
-                    System.out.print(letra);
+                    escritura.write(letra);
                 }
 
                 c = archivo_lectura.read();
             }
+            escritura.close();
         } catch (IOException e) {
             System.out.println("Error en el FileStream.");
         }
