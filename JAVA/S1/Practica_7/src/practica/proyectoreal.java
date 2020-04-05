a47e0f55b973745b4dced4cddcc347e16ac18e87
Dani Apesteguia <alumnedani@gmail.com>
2020-04-05 16:09:56 +0200

    Actualizo la opción del menú. Me queda la 3ra

--- JAVA/S1/Practica_7/src/practica/proyectoreal.java
+++ JAVA/S1/Practica_7/src/practica/proyectoreal.java
@@ -6,7 +6,7 @@ import java.io.*;
 import java.util.*;
 
 public class proyectoreal {
-
+    //PROGRAMA CON BYTES Y WRITE
     public static void byte_a_byte() {
         String[] informacion_pelicula = {"-----", "Año: ", "Director: ", "Duración: ", "Sinopsis: ", "Reparto: ", "Sesión: "};
         int recorrer = 0;
@@ -24,54 +24,90 @@ public class proyectoreal {
             while (!final_archivo) {
                 int byte_entrada = archivo_lectura.read();
                 if (byte_entrada != -1) {
-                    if (byte_entrada == 123) {
+                    if (byte_entrada == 123) { //123 BITS representan el carácter {
                         recorrer = 0;
-                    } else if (byte_entrada == 35 || recorrer == 0) {
-                        if (recorrer == 1){
+                    } else if (byte_entrada == 35 || recorrer == 0) { //35 BITS representan el carácter #
+                        if (recorrer == 1) {
                             archivo_creado.write(informacion_pelicula[0].getBytes("UTF-8"));
                             archivo_creado.write(13);
                             archivo_creado.write(13);
                             archivo_creado.write(informacion_pelicula[recorrer].getBytes("UTF-8"));
-                        }
-                        else {
+                        } else {
                             archivo_creado.write(13);
                             archivo_creado.write(13);
                             archivo_creado.write(informacion_pelicula[recorrer].getBytes("UTF-8"));
-                            if(byte_entrada != 35){
+                            if (byte_entrada != 35) {
                                 archivo_creado.write(byte_entrada);
                             }
-                            
+
                         }
                         recorrer++;
-                        
-                    }
-                    else{
+
+                    } else {
                         archivo_creado.write(byte_entrada);
                     }
-                }
-            else{
+                } else {
                     final_archivo = true;
                 }
-        }
-    }
-    catch (IOException ex
-
-    
-        ) {
+            }
+        } catch (IOException ex) {
             System.out.print("Error en la lectura del archivo: " + ex);
+        }
 
     }
+    //PROGRAMA CON CHAR Y PRINTWRITER
+    public static void caracter_a_caracter() {
+        String[] informacion_pelicula = {"-----", "Año: ", "Director: ", "Duración: ", "Sinopsis: ", "Reparto: ", "Sesión: "};
+        int recorrer = 0;
+        try {
+            FileInputStream archivo_lectura = new FileInputStream("C:/Users/alumn/Desktop/X/Programacion/JAVA/S1/Practica_7/src/practica/rafa.txt");
+            FileWriter escritura = new FileWriter("C:/Users/alumn/Desktop/X/Programacion/JAVA/S1/Practica_7/src/practica/rafaBeta.txt");
+            PrintWriter printwriter = new PrintWriter(escritura);
+            
+            printwriter.println("--------------------------------------");
+            printwriter.println(" ");
+            printwriter.println("Cartelera de CineFBMoll");
+            printwriter.println(" ");
+            printwriter.println("--------------------------------------");
+            int c = 0;
+            while (c != -1) {
+                char letra = (char) c; //Convierte el valor en bits en su carácter
+                if (letra == 123) { //El carácter 123 es el carácter {
+                    recorrer = 0;
+                } else if (c == 35 || recorrer == 0) { //El carácter 35 es el carácter #
+                    if (recorrer == 1) { //Para imprimir las letras de la película
+                        printwriter.print("-------");
+                        printwriter.println(" ");
+                        printwriter.println(" ");
+                        printwriter.print(informacion_pelicula[recorrer]);
+                    } else { //Para poner si es el título, año, director, género...)
+                        printwriter.println(" ");
+                        printwriter.println(" ");
+                        printwriter.print(informacion_pelicula[recorrer]);
+                        if (letra != 35) { //Esta condición es para imprimir la primera letra de la película.
+                            printwriter.print(letra);
+                        }
+                    }
+                    recorrer++;
+                } else {
+                    escritura.write(letra);
+                }
 
-}
-
-public static void caracter_a_caracter(){
-    }
+                c = archivo_lectura.read();
+            }
+            escritura.close();
+        } catch (IOException ex) {
+            System.out.println("Error en el FileStream:"+ex);
+        }
     
-    public static void linea_a_linea(){
-        
     }
-    
-/*    public String introFicheroEntrada(){
+
+    //PROGRAMA CON BUFFER Y LINEA A LINEA
+    public static void linea_a_linea() {
+
+    }
+
+    /*    public String introFicheroEntrada(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Introduce el fichero de entrada:");
         fichero_entrada = sc.next();
@@ -84,7 +120,7 @@ public static void caracter_a_caracter(){
         return fichero_salida;
     }
     
-*/    
+     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("--------- MENU ---------");
@@ -94,7 +130,7 @@ public static void caracter_a_caracter(){
         int opcion = sc.nextInt();
         switch (opcion) {
             case 1:
-                byte_a_byte();  
+                byte_a_byte();
                 break;
             case 2:
                 caracter_a_caracter();
